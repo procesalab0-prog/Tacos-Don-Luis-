c40f3756323f021b907de2b3ef043b435364630e
procesalab0-prog <procesalab0@gmail.com>
2026-08-09 17:42:18 -0600

    chore: finalize PWA v2.5.1 deployment

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+self.__DON_LUIS_PWA_VERSION__='2.5.1';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',()=>{});
