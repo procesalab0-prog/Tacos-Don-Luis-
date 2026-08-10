31971143a240b457af9dde5739ee1230ad1f557c
procesalab0-prog <procesalab0@gmail.com>
2026-08-09 19:36:54 -0600

    chore: despliega versión final 2.5.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.__DON_LUIS_PWA_VERSION__='2.5.3';\n// release-sync: 2.5.3
-self.addEventListener('install',()=>self.skipWaiting());
+self.__DON_LUIS_PWA_VERSION__='2.5.3';\nself.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',()=>{});
+// release-sync: 2.5.3-final
