5d26d407f68290f87ade4f1f3ffd19a7899f71a2
procesalab0-prog <procesalab0@gmail.com>
2026-08-10 15:25:27 -0600

    fix: renueva sesiones y endurece accesos en versión 2.6.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-self.__DON_LUIS_PWA_VERSION__='2.5.4';
-// release-sync: 2.5.4
+self.__DON_LUIS_PWA_VERSION__='2.6.0';
+// release-sync: 2.6.0
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',()=>{});
