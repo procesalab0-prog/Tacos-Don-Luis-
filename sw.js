1955479b73501eaa4c109ca25e038fd323477b39
procesalab0-prog <procesalab0@gmail.com>
2026-08-10 19:39:33 -0600

    feat: actualiza experiencia de pedido 2.11.9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-self.__DON_LUIS_PWA_VERSION__='2.11.8';
-// release-sync: 2.11.8
+self.__DON_LUIS_PWA_VERSION__='2.11.9';
+// release-sync: 2.11.9
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',()=>{});
